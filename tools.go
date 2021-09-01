d60c667362f8589422b0794274c11ffc2c948307
James Read <contact@jread.com>
2021-09-01 08:43:58 +0100

    Add buf

--- tools.go
+++ tools.go
@@ -3,6 +3,7 @@
 package tools
 
 import (
+    _ "github.com/bufbuild/buf/cmd/buf"
     _ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway"
     _ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2"
     _ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
