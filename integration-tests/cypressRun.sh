b72e351c694be5827929073e0a629f72449fd2a1
jamesread <contact@jwread.com>
2021-10-18 08:48:58 +0100

    cypressRun

--- integration-tests/cypressRun.sh
+++ integration-tests/cypressRun.sh
@@ -4,7 +4,7 @@ set -o xtrace
 
 echo "Running config $1"
 
-cp -f config/config{.$1,}.yaml
+cp -f ../configs/config.{$1}.yaml ./config/config.yaml
 docker start olivetin
 NO_COLOR=1 ./node_modules/.bin/cypress run --headless -s cypress/integration/$1/*  || true
 docker kill olivetin
