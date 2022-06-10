f9b6d60ca8c4bef4244522ca30523746e4e996e7
jamesread <contact@jread.com>
2022-06-10 19:25:23 +0100

    cicd: integration test for hidden footer

---
+++ integration-tests/cypress/integration/hiddenFooter/hiddenFooter.spec.js
@@ -0,0 +1,14 @@
+describe('Hidden Footer', () => {
+  beforeEach(() => {
+    cy.visit("/")
+    cy.wait(500)
+  });
+
+  it('Footer is hidden', () => {
+    cy.get('footer').then($el => {
+      expect(Cypress.dom.isHidden($el)).to.be.true
+    })
+  })
+});
+
+
