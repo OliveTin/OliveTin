21b33086aecbbcecb907f4d8716276518f2b7e26
jamesread <contact@jread.com>
2022-11-13 07:34:23 +0000

    fmt: ST1005 https://staticcheck.io/docs/checks/#ST1005

--- internal/httpservers/restapi.go
+++ internal/httpservers/restapi.go
@@ -27,7 +27,7 @@ func parseToken(cookieValue string) (*jwt.Token, error) {
 	return jwt.Parse(cookieValue, func(token *jwt.Token) (interface{}, error) {
 		// Don't forget to validate the alg is what you expect:
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
 
 		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
