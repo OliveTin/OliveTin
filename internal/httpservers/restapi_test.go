f07e6ad09faae0165a279ff4ba73c5db66eec4ce
Bernard Crnković <bernard.crnkovic@happening.xyz>
2023-05-22 14:36:57 +0200

    fix: test works

--- internal/httpservers/restapi_test.go
+++ internal/httpservers/restapi_test.go
@@ -20,24 +20,27 @@ import (
 	"time"
 )
 
-func TestJWTSignatureVerificationSucceeds(t *testing.T) {
+func testBase(t *testing.T, expire int64, expectCode int) {
 	tmpFile, _ := os.CreateTemp(os.TempDir(), "olivetin-jwt-")
-	//defer os.Remove(tmpFile.Name())
+	defer os.Remove(tmpFile.Name())
 
 	fmt.Println("Created File: " + tmpFile.Name())
 
 	privateKey, _ := rsa.GenerateKey(rand.Reader, 2048)
 	pubKey := &privateKey.PublicKey
+	// https://stackoverflow.com/questions/13555085/save-and-load-crypto-rsa-privatekey-to-and-from-the-disk
+	pkixPubKey, _ := x509.MarshalPKIXPublicKey(pubKey)
 	pubPem := pem.EncodeToMemory(
 		&pem.Block{
-			Type:  "PUBLIC KEY",
-			Bytes: x509.MarshalPKCS1PublicKey(pubKey),
+			Type:  "RSA PUBLIC KEY",
+			Bytes: pkixPubKey,
 		},
 	)
 	if err := os.WriteFile(tmpFile.Name(), pubPem, 0755); err != nil {
 		fmt.Printf("error when dumping pubKey: %s \n", err)
 	}
-	// default config + overrides, don't know how to pass it to rest server to use it
+
+	// default config + overrides
 	config := config2.DefaultConfig()
 	config.AuthJwtPubKeyPath = tmpFile.Name()
 	config.AuthJwtClaimUsername = "sub"
@@ -46,8 +49,10 @@ func TestJWTSignatureVerificationSucceeds(t *testing.T) {
 	SetGlobalRestConfig(config) // ugly, setting global var, we should pass configs as params to modules... :/
 
 	token := jwt.New(jwt.SigningMethodRS256)
+
 	claims := token.Claims.(jwt.MapClaims)
-	claims["exp"] = time.Now().Add(10 * time.Minute)
+	claims["nbf"] = time.Now().Unix() - 1000
+	claims["exp"] = time.Now().Unix() + expire
 	claims["sub"] = "test"
 	claims["olivetinGroup"] = "test"
 
@@ -67,6 +72,9 @@ func TestJWTSignatureVerificationSucceeds(t *testing.T) {
 	)
 	mux.HandlePath("GET", "/", func(w http.ResponseWriter, r *http.Request, pathParams map[string]string) {
 		username, usergroup := parseJwtCookie(r)
+		if username == "" {
+			w.WriteHeader(403)
+		}
 		w.Write([]byte(fmt.Sprintf("username=%v, usergroup=%v", username, usergroup)))
 	})
 
@@ -91,11 +99,15 @@ func TestJWTSignatureVerificationSucceeds(t *testing.T) {
 	req.AddCookie(cookie)
 	res, _ := client.Do(req)
 	defer res.Body.Close()
-	assert.Equal(t, 200, res.StatusCode)
+	assert.Equal(t, expectCode, res.StatusCode)
 	body, _ := io.ReadAll(res.Body)
 	fmt.Println(string(body))
 }
 
-func TestJWTSignatureVerificationFails(t *testing.T) {
+func TestJWTSignatureVerificationSucceeds(t *testing.T) {
+	testBase(t, 1000, 200)
+}
 
+func TestJWTSignatureVerificationFails(t *testing.T) {
+	testBase(t, -500, 403)
 }
