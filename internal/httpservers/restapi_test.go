58be9fb5fc36c3d3e0c8b6d1bb0fc298b1602137
Bernard Crnković <bernard.crnkovic@happening.xyz>
2023-05-22 13:01:42 +0200

    wip: e2e test example

---
+++ internal/httpservers/restapi_test.go
@@ -0,0 +1,106 @@
+package httpservers
+
+import (
+	"context"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	config2 "github.com/OliveTin/OliveTin/internal/config"
+	"github.com/OliveTin/OliveTin/internal/cors"
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/protobuf/encoding/protojson"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"os"
+	"testing"
+	"time"
+)
+
+func TestJWTSignatureVerificationSucceeds(t *testing.T) {
+	tmpFile, _ := os.CreateTemp(os.TempDir(), "olivetin-jwt-")
+	defer os.Remove(tmpFile.Name())
+
+	fmt.Println("Created File: " + tmpFile.Name())
+
+	privateKey, _ := rsa.GenerateKey(rand.Reader, 2048)
+	pubKey := &privateKey.PublicKey
+	pubPem := pem.EncodeToMemory(
+		&pem.Block{
+			Type:  "RSA PUBLIC KEY",
+			Bytes: x509.MarshalPKCS1PublicKey(pubKey),
+		},
+	)
+	if err := os.WriteFile(tmpFile.Name(), pubPem, 0755); err != nil {
+		fmt.Printf("error when dumping pubKey: %s \n", err)
+	}
+	// default config + overrides, don't know how to pass it to rest server to use it
+	config := config2.DefaultConfig()
+	config.AuthJwtPubKeyPath = tmpFile.Name()
+	config.AuthJwtClaimUsername = "sub"
+	config.AuthJwtClaimUserGroup = "olivetinGroup"
+	config.AuthJwtCookieName = "authorization_token"
+
+	token := jwt.New(jwt.SigningMethodRS256)
+	claims := token.Claims.(jwt.MapClaims)
+	claims["exp"] = time.Now().Add(10 * time.Minute)
+	claims["olivetinGroup"] = "test"
+	claims["sub"] = "test"
+
+	tokenStr, _ := token.SignedString(privateKey)
+
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	// init mux endpoint like in restapi.go (but using dummy response handler)
+	mux := runtime.NewServeMux(
+		runtime.WithMetadata(parseRequestMetadata), // i am guessing this is critical middleware for authorizing request cookie
+		runtime.WithMarshalerOption(runtime.MIMEWildcard, &runtime.HTTPBodyMarshaler{
+			Marshaler: &runtime.JSONPb{
+				MarshalOptions: protojson.MarshalOptions{
+					UseProtoNames:   true,
+					EmitUnpopulated: true,
+				},
+			},
+		}),
+	)
+	mux.HandlePath("GET", "/", func(w http.ResponseWriter, r *http.Request, pathParams map[string]string) {
+		// username, usergroup := parseJwtCookie(r)
+		username, usergroup := "a", "b"
+		w.Write([]byte(fmt.Sprintf("username=%v, usergroup=%v", username, usergroup)))
+	})
+
+	// make server and attach handler
+	srv := &http.Server{Handler: cors.AllowCors(mux)}
+	lis, _ := net.Listen("tcp", ":1337")
+
+	go func() {
+		if err := srv.Serve(lis); err != nil {
+			t.Errorf("couldn't start server: %v", err)
+		}
+	}()
+
+	// make http client and send request to myself
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", "http://localhost:1337/", nil)
+	cookie := &http.Cookie{
+		Name:   "authorization_token",
+		Value:  tokenStr,
+		MaxAge: 300,
+	}
+	req.AddCookie(cookie)
+	res, _ := client.Do(req)
+	defer res.Body.Close()
+	assert.Equal(t, 200, res.StatusCode)
+	body, _ := ioutil.ReadAll(res.Body)
+	fmt.Println(string(body))
+}
+
+func TestJWTSignatureVerificationFails(t *testing.T) {
+
+}
