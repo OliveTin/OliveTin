2ada67be04db31ee3fc24fe9eab44de03aafcb5e
James Read <contact@jread.com>
2023-10-09 22:44:29 +0100

    Migrate integration-test infrastucture from Cypress to Selenium+Mocha (#170)
    * cicd: Move cypress to selenium+mocha
    
    * cicd: with is not supported unless using an a container
    
    * cicd: relative path to mocha
    
    * cicd: the integration-tests runner now starts/stops OliveTin
    
    * cicd: Knowing the CWD helps debugging
    
    * cicd: Headless chrome
    
    * cicd: wait for integration-test server to be started
    
    * fmt: Mostly fix isses from eslint
    
    * cicd: #169 - Test multiple combo boxes
    
    * fmt: let should be const
    
    * cicd: Remove cypress entirely

---
+++ integration-tests/test/hiddenFooter.mjs
@@ -0,0 +1,21 @@
+import { expect } from 'chai';
+
+import { By } from 'selenium-webdriver';
+
+describe('config: hiddenFooter', function () {
+  before(async function () {
+    await runner.start('hiddenFooter')
+  });
+
+  after(async () => {
+    await runner.stop()
+  });
+
+  it('Check that footer is hidden', async () => {
+    await webdriver.get('http://localhost:1337')
+
+    let footer = await webdriver.findElement(By.tagName('footer'))
+
+    expect(await footer.isDisplayed()).to.be.false
+  })
+})
