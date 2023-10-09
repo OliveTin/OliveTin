6f3677b3f6d3b2a1155ab848920102a3f691f073
jamesread <contact@jread.com>
2023-10-09 09:58:52 +0100

    cicd: Headless chrome

--- integration-tests/mochaSetup.mjs
+++ integration-tests/mochaSetup.mjs
@@ -1,11 +1,16 @@
-import {By, Builder, Browser} from 'selenium-webdriver';
-import getRunner from './runner.mjs';
+import { Options } from 'selenium-webdriver/chrome.js'
+import { Builder, Browser } from 'selenium-webdriver'
+import getRunner from './runner.mjs'
 
-export async function mochaGlobalSetup() {
-  global.webdriver = await new Builder().forBrowser('chrome').build();
+export async function mochaGlobalSetup () {
+  const options = new Options()
+  options.addArguments('--headless')
+
+  global.webdriver = await new Builder().forBrowser(Browser.CHROME).setChromeOptions(options).build()
+
   global.runner = getRunner()
 }
 
-export async function mochaGlobalTeardown() {
+export async function mochaGlobalTeardown () {
   await global.webdriver.quit();
 }
