6b3e9e4676ace3d327294145edd326e0247471ee
James Read <contact@jread.com>
2023-12-28 23:48:27 +0000

    feature: clock and ashtonished emoji (#207)

--- internal/config/emoji.go
+++ internal/config/emoji.go
@@ -1,13 +1,15 @@
 package config
 
 var emojis = map[string]string{
-	"":        "&#x1F600;", // default icon
-	"poop":    "&#x1f4a9;",
-	"smile":   "&#x1F600;",
-	"ping":    "&#x1f4e1;",
-	"backup":  "&#128190;",
-	"reboot":  "&#9211;",
-	"restart": "&#9211;",
+	"":            "&#x1F600;", // default icon
+	"poop":        "&#x1f4a9;",
+	"smile":       "&#x1F600;",
+	"ping":        "&#x1f4e1;",
+	"backup":      "&#128190;",
+	"reboot":      "&#9211;",
+	"restart":     "&#9211;",
+	"ashtonished": "&#128562;",
+	"clock":       "&#128338;",
 }
 
 func lookupHTMLIcon(keyToLookup string) string {
