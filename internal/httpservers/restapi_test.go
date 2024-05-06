50c74701025cc4e79cb02677b11cac71f64307f2
jamesread <contact@jread.com>
2024-05-06 22:37:13 +0100

    fmt: Clean REST API Testing

--- internal/httpservers/restapi_test.go
+++ internal/httpservers/restapi_test.go
@@ -1,5 +1,11 @@
 package httpservers
 
+/*
+The REST API actually has very few tests, as the "real" API behind OliveTin
+is is implemented as a gRPC in /internal/grpc. The REST API therefore only
+handles HTTP specific stuff like authentication cookies and JWT parsing.
+*/
+
 import (
 	"fmt"
 	"github.com/OliveTin/OliveTin/internal/cors"
