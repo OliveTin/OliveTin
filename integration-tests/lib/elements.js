5a708e69e4b3c91c0f6f5c5246bbb9d039584b58
jamesread <contact@jread.com>
2024-06-02 23:09:11 +0100

    cicd: Better debugging for domStatus

--- integration-tests/lib/elements.js
+++ integration-tests/lib/elements.js
@@ -31,14 +31,15 @@ export async function requireExecutionDialogStatus (webdriver, expected) {
   const domStatus = await webdriver.findElement(By.id('execution-dialog-status'))
 
   // It seems that webdriver will not give us text if domStatus is hidden (which it will be until complete)
-  await webdriver.executeScript("window.executionDialog.domExecutionDetails.hidden = false")
+  await webdriver.executeScript('window.executionDialog.domExecutionDetails.hidden = false')
 
-  await webdriver.wait(new Condition('wait for action to be running', async function() {
+  await webdriver.wait(new Condition('wait for action to be running', async function () {
     const actual = await domStatus.getText()
 
-    if (actual == expected) {
+    if (actual === expected) {
       return true
     } else {
+      console.log('Waiting for domStatus text to be: ', expected, ', it is currently: ', actual)
       return false
     }
   }))
