ebc6d57c61fdaef2600a602b2bb68123f46b34ec
wushuzh <wushuzh@outlook.com>
2024-07-23 10:56:01 +0800

    bugfix: change to correct type of act req in TC

--- integration-tests/test/prometheus.mjs
+++ integration-tests/test/prometheus.mjs
@@ -4,7 +4,7 @@ import { expect } from 'chai'
 import { By } from 'selenium-webdriver'
 
 let metrics = [
-  {'name': 'olivetin_actions_requested_count', 'type': 'gauge', 'desc': 'The actions requested count'},
+  {'name': 'olivetin_actions_requested_count', 'type': 'counter', 'desc': 'The actions requested count'},
   {'name': 'olivetin_config_action_count', 'type': 'gauge', 'desc': 'The number of actions in the config file'},
   {'name': 'olivetin_config_reloaded_count', 'type': 'counter', 'desc': 'The number of times the config has been reloaded'},
   {'name': 'olivetin_sv_count', 'type': 'gauge', 'desc': 'The number entries in the sv map'},
