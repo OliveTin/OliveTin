274d036f7473fcdfba6313133b49f8532e33d7fd
James Read <contact@jread.com>
2024-08-09 20:04:57 +0100

    fmt: Cleanup terminal code (#381)
    * fmt: Clean up OutputTerminal code
    
    * fmt: Clean up OutputTerminal code

---
+++ webui.dev/js/OutputTerminal.js
@@ -0,0 +1,30 @@
+import { Terminal } from '@xterm/xterm'
+import { FitAddon } from '@xterm/addon-fit'
+
+export class OutputTerminal {
+  constructor () {
+    this.terminal = new Terminal({
+      convertEol: true
+    })
+
+    const fitAddon = new FitAddon()
+    this.terminal.loadAddon(fitAddon)
+    this.terminal.fit = fitAddon
+  }
+
+  write (out, then) {
+    this.terminal.write(out, then)
+  }
+
+  fit () {
+    this.terminal.fit.fit()
+  }
+
+  open (el) {
+    this.terminal.open(el)
+  }
+
+  reset () {
+    this.terminal.reset()
+  }
+}
