c20eea29cde8460ff90dd8d6a3f713ee2af21b6c
James Read <contact@jread.com>
2025-05-26 08:33:22 +0100

    feat: win unicode flag (#590)

--- service/internal/executor/executor_windows.go
+++ service/internal/executor/executor_windows.go
@@ -6,6 +6,7 @@ package executor
 import (
 	"context"
 	"os/exec"
+	"os"
 )
 
 func (e *Executor) Kill(execReq *InternalLogEntry) error {
@@ -13,5 +14,11 @@ func (e *Executor) Kill(execReq *InternalLogEntry) error {
 }
 
 func wrapCommandInShell(ctx context.Context, finalParsedCommand string) *exec.Cmd {
-	return exec.CommandContext(ctx, "cmd", "/C", finalParsedCommand)
+	winCodepage := os.Getenv("OT_WIN_FLAG_U")
+
+	if winCodepage == "0" {
+		return exec.CommandContext(ctx, "cmd", "/C", finalParsedCommand)
+	} else {
+		return exec.CommandContext(ctx, "cmd", "/u", "/C", finalParsedCommand)
+	}
 }
