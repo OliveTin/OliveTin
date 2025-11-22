4ad481eb9f92691f06f75054d3b7aea435dc0c6d
jamesread <contact@jread.com>
2025-11-22 09:30:01 +0000

    chore: dep bump

---
+++ webui.dev/eslint.config.mjs
@@ -0,0 +1,27 @@
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([{
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+        },
+
+        ecmaVersion: 12,
+        sourceType: "module",
+    },
+
+    rules: {},
+}]);
