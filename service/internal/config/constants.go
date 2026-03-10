86c35f40c324106d62d98be145aefe7220126473
jamesread <contact@jread.com>
2026-03-10 23:28:17 +0000

    chore: remove unneeded comment

--- service/internal/config/constants.go
+++ service/internal/config/constants.go
@@ -1,5 +1,3 @@
 package config
 
-// ContentSecurityPolicyDefault is the default Content-Security-Policy header value
-// when security headers are enabled and no custom policy is set.
 const ContentSecurityPolicyDefault = "default-src 'self'; script-src 'self' 'unsafe-inline' https:; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; connect-src 'self' https:; frame-ancestors 'none'; base-uri 'self'"
